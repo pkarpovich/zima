9cd8fb86d800c61d1a2ee85c1196b06317a3a078
Pavel Karpovich <pavel.karpovich@outlook.com>
2021-12-11 23:33:38 +0000

    wip: run server request with retry

--- microservices/spotify-service/src/index.mjs
+++ microservices/spotify-service/src/index.mjs
@@ -37,47 +37,58 @@ await spotifyService.init();
 
 const serviceQueueName = configService.get("Rabbit.SpotifyQueueName");
 
+const runFunctionWithRetry = async (func) => {
+  try {
+    await func()
+  } catch {
+    await spotifyService.refreshAccess();
+    await func()
+  }
+}
+
 const handleQueueMessage = (_, channel) => async (msg) => {
   const { name, props } = JSON.parse(msg.content.toString());
   let ansibleOutput = {};
 
-  const devices = await spotifyService.getDevices();
-  const tv = devices.find((d) => d.type === "TV");
+  await runFunctionWithRetry(async () => {
+    const devices = await spotifyService.getDevices();
+    const tv = devices.find((d) => d.type === "TV");
 
-  switch (name) {
-    case ActionTypes.Spotify.Resume: {
-      await spotifyService.resume(tv.id);
-      break;
+    switch (name) {
+      case ActionTypes.Spotify.Resume: {
+        await spotifyService.resume(tv.id);
+        break;
+      }
+      case ActionTypes.Spotify.Pause: {
+        await spotifyService.pause(tv.id);
+        break;
+      }
+      case ActionTypes.Spotify.NextTrack: {
+        await spotifyService.nextTrack(tv.id);
+        break;
+      }
+      case ActionTypes.Spotify.PrevTrack: {
+        await spotifyService.prevTrack(tv.id);
+        break;
+      }
+      case ActionTypes.Spotify.RestartTack: {
+        await spotifyService.seekTrack(0, tv.id);
+        break;
+      }
+      case ActionTypes.Spotify.PlayPlaylist: {
+        await spotifyService.play(tv.id, props[0]);
+        break;
+      }
+      case ActionTypes.Spotify.EnableShuffle: {
+        await spotifyService.setShuffle(tv.id, true);
+        break;
+      }
+      case ActionTypes.Spotify.DisableShuffle: {
+        await spotifyService.setShuffle(tv.id, false);
+        break;
+      }
     }
-    case ActionTypes.Spotify.Pause: {
-      await spotifyService.pause(tv.id);
-      break;
-    }
-    case ActionTypes.Spotify.NextTrack: {
-      await spotifyService.nextTrack(tv.id);
-      break;
-    }
-    case ActionTypes.Spotify.PrevTrack: {
-      await spotifyService.prevTrack(tv.id);
-      break;
-    }
-    case ActionTypes.Spotify.RestartTack: {
-      await spotifyService.seekTrack(0, tv.id);
-      break;
-    }
-    case ActionTypes.Spotify.PlayPlaylist: {
-      await spotifyService.play(tv.id, props[0]);
-      break;
-    }
-    case ActionTypes.Spotify.EnableShuffle: {
-      await spotifyService.setShuffle(tv.id, true);
-      break;
-    }
-    case ActionTypes.Spotify.DisableShuffle: {
-      await spotifyService.setShuffle(tv.id, false);
-      break;
-    }
-  }
+  })
 
   const serviceResp = JSON.stringify(ansibleOutput);
 
